5deee408932b934aad0d044c8a9ffa594c5bc4a5
Yttehs <shetty.yttehs@outlook.com>
2025-05-27 17:54:08 +0800

    chore: remove sample code

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,14 @@
 
 use embassy_executor::Spawner;
 use embassy_time::Timer;
-use ch32_hal::gpio::{AnyPin, Level, Output, Pin};
 use ch32_hal::println;
 use panic_halt as _;
 
-#[embassy_executor::task]
-async fn blink(pin: AnyPin, interval_ms: u64) {
-    let mut led = Output::new(pin, Level::Low, Default::default());
-
-    loop {
-        led.set_high();
-        Timer::after_millis(interval_ms).await;
-        led.set_low();
-        Timer::after_millis(interval_ms).await;
-    }
-}
-
 #[embassy_executor::main(entry = "qingke_rt::entry")]
-async fn main(spawner: Spawner) -> ! {
+async fn main(_spawner: Spawner) -> ! {
     ch32_hal::debug::SDIPrint::enable();
     let p = ch32_hal::init(ch32_hal::Config::default());
 
-    // Adjust the LED GPIO according to your board
-    spawner.spawn(blink(p.PA0.degrade(), 1000)).unwrap();
     loop {
         Timer::after_millis(1000).await;
         println!("tick");
